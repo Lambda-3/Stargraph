92429e6ee580bd61b0c79d5c02a6e792e2a06cbf
Matthias Cetto <zo.matthias@gmail.com>
2017-07-14 16:52:09 +0200

    Added new CoreferenceResolutionProcessor for documents.

---
+++ stargraph-core/src/main/java/net/stargraph/core/processors/CoreferenceResolutionProcessor.java
@@ -0,0 +1,69 @@
+package net.stargraph.core.processors;
+
+/*-
+ * ==========================License-Start=============================
+ * stargraph-core
+ * --------------------------------------------------------------------
+ * Copyright (C) 2017 Lambda^3
+ * --------------------------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * ==========================License-End===============================
+ */
+
+import com.typesafe.config.Config;
+import net.stargraph.data.processor.BaseProcessor;
+import net.stargraph.data.processor.Holder;
+import net.stargraph.data.processor.ProcessorException;
+import net.stargraph.model.*;
+import org.lambda3.graphene.core.Graphene;
+import org.lambda3.graphene.core.coreference.model.CoreferenceContent;
+
+import java.io.Serializable;
+
+/**
+ * Can be placed in the workflow to resolve coreferences.
+ */
+public final class CoreferenceResolutionProcessor extends BaseProcessor {
+    public static String name = "coref-processor";
+
+    private static final Graphene GRAPHENE = new Graphene();
+
+    public CoreferenceResolutionProcessor(Config config) {
+        super(config);
+    }
+
+    @Override
+    public void doRun(Holder<Serializable> holder) throws ProcessorException {
+        Serializable entry = holder.get();
+
+        if (entry instanceof Document) {
+            Document document = (Document)entry;
+
+            CoreferenceContent cc = GRAPHENE.doCoreference(document.getText());
+            String resolved = cc.getSubstitutedText();
+
+            holder.set(new Document(document.getTitle(), resolved));
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
